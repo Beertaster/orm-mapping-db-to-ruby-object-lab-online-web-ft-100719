1dd0089cb740767f47a96a37fa4ae871e26c7957
William Stevens <wstevens.hornet@gmail.com>
2019-11-15 22:32:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -110,7 +110,7 @@ class Student
       end.first
   end
   
-  def self.all_students_in_grade_X(grade)
+  def self.all_students_in_grade_x(grade)
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = ?
