3db96263ad816854f2c29ddbdd200c49ecb22efd
William Stevens <wstevens.hornet@gmail.com>
2019-11-15 20:39:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,11 +77,17 @@ class Student
   def self.students_below_12th_grade 
     sql = <<-SQL
     SELECT * FROM students
-    WHERE grade < 12
+    WHERE grade IS NOT 12
     SQL
     
     DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
+  end
+  
+  def self.first_X_students_in_grade_10(number)
+    
+    
+    
   end
   
   
