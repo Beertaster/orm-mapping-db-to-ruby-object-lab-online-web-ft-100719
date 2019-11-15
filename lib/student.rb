fc56d84f41f47740970f6b428af9480dedb04a68
William Stevens <wstevens.hornet@gmail.com>
2019-11-15 22:19:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -111,7 +111,15 @@ class Student
   end
   
   def self.all_students_in_grade_X(number)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 1?
+    LIMIT 1
+    SQL
     
+    DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end.first
   end
   
   
